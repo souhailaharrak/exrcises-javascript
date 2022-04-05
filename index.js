852b79bc1c293a18b02acae1cff514878f5e2a08
souhailaharrak <92025114+souhailaharrak@users.noreply.github.com>
2022-04-05 16:26:59 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// debut du RAND;
+// let debutrRND=15;
+// let debutMInutRnd=40;
+// // fin de JOURNe
+//  let finJOURRandheur=16;
+// let findJOURMinutRand=30;
+// let dure=15;
+// // calcule de la fin de RDV
+// let finRandheur=debutrRND;
+// let findMinutRand=debutMInutRnd+dure;
+// if(finRandheur<finJOURRandheur){
+// console.log("RAND OK");
+// }else{
+//     console.log("RAnd impossible");
+// }
+// console.log(`le RDV se terminera à ${finRandheur} h +${findMinutRand} min`);
+
+
+// function coding(chiffre){
+//     if(chiffre==0){
+//        return "zero";
+//     }else if(chiffre==1){
+//        return "un";
+//     }else{
+//        return "deux"
+//     }
+// } 
+// coding();
+// function coding(chiffre){
+//     return ["zero","un","deux"][chiffre];
+//   } 
+//   coding(0);
+
+// function num(n){
+
+//     if(namberPair(n)){
+//         n=n/2;
+ 
+//     }else{
+//         n=n*3+1;
+//     }console.log(n)
+// }
+
+// function namberPair(nombrt){
+ 
+//   return nombrt % 2===0;
+// }num(3);
+
